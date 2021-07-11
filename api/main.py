10ac3f0bc2545813830de618a3ab479abbb89f5c
Valerie Lin <vallinfriends@gmail.com>
2021-07-10 21:44:56 -0400

    enabled pylint and black code formatter in python

--- api/main.py
+++ api/main.py
@@ -11,25 +11,24 @@ UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
 DEBUG = bool(os.environ.get("DEBUG", True))
 
 if not UNSPLASH_KEY:
-  raise EnvironmentError("Please create .env.local file and insert UNSPLASH_KEY")
+    raise EnvironmentError("Please create .env.local file and insert UNSPLASH_KEY")
 
 app = Flask(__name__)
 CORS(app)
 
 app.config["DEBUG"] = DEBUG
 
+
 @app.route("/new-image")
 def new_image():
-  word = request.args.get("query")
-  headers = {
-    "Accept-Version": "v1",
-    "Authorization": "Client-ID " + UNSPLASH_KEY
-  }
-  params = {"query": word}
-  response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
-  
-  data = (response.json())
-  return data
+    word = request.args.get("query")
+    headers = {"Accept-Version": "v1", "Authorization": "Client-ID " + UNSPLASH_KEY}
+    params = {"query": word}
+    response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
+
+    data = response.json()
+    return data
+
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5050)
+    app.run(host="0.0.0.0", port=5050)
